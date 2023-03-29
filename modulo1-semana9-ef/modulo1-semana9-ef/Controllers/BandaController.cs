c9bf45f8446de8a1302e17fee1aa1f781b617599
Delson Magalhaes <delson_magalhaes@estudante.sesisenai.org.br>
2023-03-29 19:18:08 -0300

    commit inicial

---
+++ modulo1-semana9-ef/modulo1-semana9-ef/Controllers/BandaController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace modulo1_semana9_ef.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BandaController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult Post()
+        {
+            return Ok(true);
+        }
+    }
+}
